d0a938d0c20c0a5d59ef602c6730f6da7471984f
student <student@10.10.23.52>
2015-11-29 17:02:58 +0100

    Damian commit

--- zpi_lab1/dist/js/bootstrap.js
+++ zpi_lab1/dist/js/bootstrap.js
@@ -25,7 +25,7 @@ Loba dodał!! *
  * Copyright 2011-2015 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * ======================================================================== 
- * Łukasz Łoba ma zle
+ * Łukasz Łoba ma dobrze
  * 
  * */
 
